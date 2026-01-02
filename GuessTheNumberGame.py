69314c913acd7cbf4e69ad4b6802a0f7bb2100b9
Arka Sahoo <arkasahoo2005@gmail.com>
2026-01-01 22:24:14 -0600

    Added code

--- GuessTheNumberGame.py
+++ GuessTheNumberGame.py
@@ -1,1 +1,28 @@
-print("Guess the number game")
+import random
+
+def number_guessing_game():
+    """
+    A number guessing game
+    """
+    generate_number = random.randint(1, 100)
+    guess = None
+    number_of_attempts = 0
+
+    while guess != generate_number:
+        try:
+            guess_number = input("Enter your guess: ")
+            guess = int(guess_number)
+            number_of_attempts += 1
+
+            if guess < generate_number:
+                print("Too low, try again.\n")
+            elif guess > generate_number:
+                print("Too high, try again.\n")
+            else:
+                break
+        except ValueError:
+            print("Invalid input. Please enter a whole number.")
+
+    print(f"You guessed the number {generate_number} correctly in {number_of_attempts} attempts!\n")
+
+number_guessing_game()
